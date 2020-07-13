8d6c7b9c922837dea7bbe2c967185cdb41610696
gadgetvala <surajj.verma7@gmail.com>
2020-07-13 13:50:35 +0530

    FIX

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './../Screens/Home/Home';
 import CountryDetails from './../Screens/CountryDetails/CountryDetail';
 
@@ -7,10 +7,10 @@ class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<>
-					<Route path="/" component={Home} />
-					<Route exact path="/:countryCode" component={CountryDetails} />
-				</>
+				<Switch>
+					<Route path="/" component={Home} exact/>
+					<Route path="/:countryCode" component={CountryDetails} exact/>
+				</Switch>
 			</BrowserRouter>
 		);
 	}
