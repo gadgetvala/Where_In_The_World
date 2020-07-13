bc1a03af19226a2ba0d7e861e7a83eb8bc3d2062
gadgetvala <surajj.verma7@gmail.com>
2020-07-13 15:59:21 +0530

    FIX

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './../Screens/Home/Home';
 import CountryDetails from './../Screens/CountryDetails/CountryDetail';
+import history from './../components/History/History';
 
 const App = () => {
 	return (
-		<BrowserRouter>
+		<BrowserRouter history={history}>
 			<Switch>
 				<Route path="/" component={Home} exact />
 				<Route path="/:countryCode" component={CountryDetails} exact />
